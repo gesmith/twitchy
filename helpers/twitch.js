ffd6abdddf24a9ba286257a443d19d1d6faa2bba
Garry Smith <ronintek0@gmail.com>
2016-08-22 12:56:00 -0400

    add channel_subscriptions scope

--- helpers/twitch.js
+++ helpers/twitch.js
@@ -3,7 +3,7 @@ var twitch = new TwitchAPI({
   clientId: process.env.TWITCHTV_CLIENT_ID,
   clientSecret: process.env.TWITCHTV_CLIENT_SECRET,
   redirectUri: process.env.TWITCHTV_REDIRECT_URI,
-  scopes: ["user_read", "channel_read", "channel_editor", "channel_commercial"]
+  scopes: ["user_read", "channel_read", "channel_editor", "channel_commercial", "channel_subscriptions"]
 });
 
 module.exports = twitch;
